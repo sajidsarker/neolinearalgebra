add9f8b226be673f8e958a8208221b8f9a63efe9
Sajid Sarker <sajid.sarker@gmail.com>
2022-09-08 16:45:43 +0430

    Initial commit

--- Matrix.py
+++ Matrix.py
@@ -1,4 +1,5 @@
-#!/usr/bin/env python3
+import math
+
 class Matrix:
     
     def __init__(self, data):
@@ -39,25 +40,34 @@ class Matrix:
         if isinstance(value, (int, float)) == False:
             raise Exception('Value must be of type int or float only.')
         self.data[row][col] = value
-    
-    def fill(self, row_range_start, row_range_end, col_range_start, col_range_end, value) -> None:
+
+    def fill(self, rows, cols, value) -> None:
         if isinstance(value, (int, float)) == False:
             raise Exception('Value must be of type int or float only.')
-	for i in range(row_range_start, row_range_end, 1):
-		for j in range(col_range_start, col_range_end, 1):
-			self.data[i][j] = value
- 
+        for i in range(rows[0], rows[1], 1):
+            for j in range(cols[0], cols[1], 1):
+                self.assign(i, j, value)
+    
     def retrieve(self, row, col) -> (int, float):
         return self.data[row][col]
 
-    def diagonal(self) -> object:
+    def diagonal(self) -> Matrix:
         matrix = []
         for i in range(self.rows):
             if i < self.cols:
                 matrix.append([self.data[i][i]])
         return Matrix(matrix)
+    
+    def magnitude(self) -> float:
+        if not (self.shape[0] == 1 or self.shape[1] == 1):
+            raise Exception('Matrix must be a row or column vector only.')
+        matrix = []
+        for i in range(self.rows):
+            for j in range(self.cols):
+                matrix.append(self.data[i][j]**2)
+        return math.sqrt(sum(matrix))
 
-    def transpose(self) -> object:
+    def transpose(self) -> Matrix:
         matrix, row = [], []
         for j in range(self.cols):
             for i in range(self.rows):
@@ -67,7 +77,7 @@ class Matrix:
         return Matrix(matrix)
 
     # TODO
-    def inverse(self) -> object:
+    def inverse(self) -> Matrix:
         if self.shape[0] != self.shape[1] and self.shape[0] < 2:
             raise Exception('Matrix is not invertible as it is a non-square matrix. Please provide a square matrix with dimensions greater than or equal to (2, 2).')
         matrix = [[1], [0]]
@@ -79,7 +89,7 @@ class Matrix:
             raise Exception('Matrix determinant cannot be derived as it is a non-square matrix. Please provide a square matrix with dimensions greater than or equal to (2, 2).')
         return 0
 
-    def __add__(self, other) -> object:
+    def __add__(self, other) -> Matrix:
         if other.shape != self.shape:
             raise Exception('Matrix dimensions are mismatched ({} != {}). Please add (2) matrices of the same dimensions.'.format(self.shape, other.shape))
         data, output = self.data, []
@@ -89,8 +99,14 @@ class Matrix:
                 output[i].append(self.data[i][j] + other.data[i][j])
         self.data = data
         return Matrix(output)
+    
+    def __ladd__(self, other) -> Matrix:
+        return self.__add__(other)
 
-    def __sub__(self, other):
+    def __radd__(self, other) -> Matrix:
+        return self.__add__(other)
+
+    def __sub__(self, other) -> Matrix:
         if other.shape != self.shape:
             raise Exception('Matrix dimensions are mismatched ({} != {}). Please subtract (2) matrices of the same dimensions.'.format(self.shape, other.shape))
         data, output = self.data, []
@@ -100,35 +116,66 @@ class Matrix:
                 output[i].append(self.data[i][j] - other.data[i][j])
         self.data = data
         return Matrix(output)
+    
+    def __lsub__(self, other) -> Matrix:
+        return self.__sub__(other)
 
-    def __mul__(self, scalar) -> object:
-        if isinstance(scalar, (int, float)) == False:
-            raise Exception('Scalar must be of type int or float only.')
+    def __rsub__(self, other) -> Matrix:
+        return self.__sub__(other)
+
+    def __mul__(self, other) -> Matrix:
+        if isinstance(other, Matrix) == False:
+            if isinstance(other, (int, float)) == False:
+                raise Exception('Expecting a matrix or a scalar of type int or float only.')
+        else:
+            if self.size != other.size:
+                raise Exception('Matrix dimensions are mismatched for pointwise matrix multiplication ({} != {})'.format(self.shape, other.shape))
+
         data, output = self.data, []
-        for i in range(self.rows):
-            output.append([])
-            for j in range(self.cols):
-                output[i].append(self.data[i][j] * scalar)
+        
+        # Scalar multiplication
+        if isinstance(other, (int, float)):
+            for i in range(self.rows):
+                output.append([])
+                for j in range(self.cols):
+                    output[i].append(self.data[i][j] * other)
+
+        # Matrix multiplication (pointwise)
+        if isinstance(other, Matrix):
+            for i in range(self.rows):
+                output.append([])
+                for j in range(self.cols):
+                    output[i].append(self.data[i][j] * other.data[i][j])
+
         self.data = data
         return Matrix(output)
 
-    def __lmul__(self, scalar) -> Matrix:
-	self.__mul__(scalar)
+    def __lmul__(self, other) -> Matrix:
+        return self.__mul__(other)
 
-    def __rmul__(self, scalar) -> Matrix:
-	self.__mul__(scalar)
+    def __rmul__(self, other) -> Matrix:
+        return self.__mul__(other)
 
-    # TODO
-    #def __mul__(self, other):
-    #    pass
-
-    # TODO
-    #def cross(self, other):
-    #    pass
-
-    # TODO
-    #def dot(self, other):
-    #    pass
+    def __matmul__(self, other) -> Matrix:
+        if self.cols != other.rows:
+            raise Exception('Matrix dimensions are mismatched for matrix multiplication ({} @ {}: {} != {})'.format(self.shape, other.shape, self.shape[1], other.shape[0]))
+        data, output = self.data, []
+        n, m = self.shape[1], self.shape[1]
+        for i in range(n):
+            output.append([])
+            for j in range(m):
+                value = 0
+                for k in range(n):
+                    value += self.data[i][k] * other.data[k][j]
+                output[i].append(value)
+        self.data = data
+        return Matrix(output)
+    
+    def __lmatmul__(self, other) -> Matrix:
+        return __matmul__(self, other)
+    
+    def __rmatmul__(self, other) -> Matrix:
+        return __matmul__(self, other)
 
     def __repr__(self) -> str:
         matrix = str(self.data).replace('],', '],\n ')
