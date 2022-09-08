212f0b2aa3338a76978a1d74e28a0c59611e794f
Sajid Sarker <sajid.sarker@gmail.com>
2022-09-08 16:56:19 +0430

    Environment setup

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='neolinearalgebra',
+      version='0.1',
+      description='Linear Algebra',
+      packages=['neolinearalgebra'],
+      zip_safe=False
+)
