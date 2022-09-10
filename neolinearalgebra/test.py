a94f03ab37fb1d70eb3d792b0a815ac8512f4a9b
Sajid Sarker <sajid.sarker@gmail.com>
2022-09-10 15:11:58 +0430

    Unit Test.

--- neolinearalgebra/test.py
+++ neolinearalgebra/test.py
@@ -2,8 +2,38 @@ import unittest
 import Matrix.py
 
 class TestMatrix(unittest.TestCase):
-	def test_1:
-		self.assert()
+    def test_matrix_1(self):
+        self.assert()
+    def test_vector_1(self):
+        self.assert()
+    def test_assign_1(self):
+        self.assert()
+    def test_fill_1(self):
+        self.assert()
+    def test_retrieve_1(self):
+        self.assert()
+    def test_diagonal_1(self):
+        self.assert()
+    def test_magnitude_1(self):
+        self.assert()
+    def test_transpose_1(self):
+        self.assert()
+    def test_determinant_1(self):
+        self.assert()
+    def test_inverse_1(self):
+        self.assert()
+    def test_add_1(self):
+        self.assert()
+    def test_sub_1(self):
+        self.assert()
+    def test_mul_1(self):
+        self.assert()
+    def test_mul_2(self):
+        self.assert()
+    def test_matmul_1(self):
+        self.assert()
+    def test_matmul_2(self):
+        self.assert()
 
 if __name__ == '__main__':
 	unittest.main()
