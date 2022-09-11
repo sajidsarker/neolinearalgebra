3bd3a76b27aad758cf198af079620632b0218fe3
Sajid Sarker <sajidsarker@users.noreply.github.com>
2022-09-11 13:39:41 +0430

    Update test.py

--- test.py
+++ test.py
@@ -2,6 +2,87 @@ import unittest
 
 from neolinearalgebra import Matrix
 
+'''
+A (check: list)
+A (check: list of lists)
+A (check: inconsistent dimensions)
+A (check: empty)
+A (check: data type)
+A (check: square matrix)
+A + A (3*3)
+A + B (3*3)
+A + C (check error) (2*2)
+A + B + A
+A - A
+A - B
+A - C (check error)
+A - B - A
+2 * A
+A * 2
+A * A
+A * B
+A + B * 3 - 2 * A + B * A
+A @ A
+A @ B
+A @ C
+A @ V
+A @ I
+I @ A
+2 * (I @ A + B)
+A + B * 3 - 2 * A + B @ A
+A.assign()
+A.assign() (check error)
+A.fill()
+A.fill() (check error)
+A.retrieve()
+A.retrieve() (check error)
+A.transpose()
+B.transpose()
+(A*B).transpose()
+(A@B).transpose()
+A.diagonal() (3*3)
+A.diagonal() (2*3)
+A.trace() (4*4)
+A.trace() (3*1)
+A.diagonal().trace()
+(A*B).transpose().trace()
+(A@B).transpose().trace()
+A.determinant()
+B.determinant()
+C.determinant()
+D.determinant([[1]])
+A.inverse()
+B.inverse() (check non invertible)
+C.inverse() (check non square)
+D.inverse() (check unimodal)
+A.inverse().transpose().trace()
+
+Commutative Law of Addition
+A+B=B+A
+Associative Law of Addition
+A+(B+C)=(A+B)+C
+Associative Law of Multiplication
+A@(B@C)=(A@B)@C
+Distributive Law of Matrix Algebra
+A@(B+C)=A@B+A@C
+k*(A+B)=k*A+k*B
+Transposition
+A.transpose().transpose()=A
+(A+B).transpose()=A.transpose()+B.transpose()
+(A@B).transpose()=B.transpose()@A.transpose()
+A@B@C=C.transpose()@B.transpose()@A.transpose()
+A@I=I@A
+A@I=A
+I@A=A
+Inversion
+A@A.inverse()=I
+A.inverse()@A=I
+A.inverse().inverse()=A
+(A@B).inverse()=B.inverse()@A.inverse()
+(A@B@C).inverse=C.inverse()@B.inverse()@A.inverse()
+A.transpose().inverse()=A.inverse().transpose()
+'''
+
 class TestMatrix(unittest.TestCase):
     def test_matrix_1(self):
         self.assert()
